d33a179259645e56f0a65d3c47e8d62c741a46b0
misha <chemike47@gmail.com>
2017-02-20 02:22:01 +0300

    Fix lambda builder

--- src/test/kotlin/jvmclassname/builder/lib.kt
+++ src/test/kotlin/jvmclassname/builder/lib.kt
@@ -43,7 +43,7 @@ fun FILE(name: String, f: NodeBuilder.() -> Unit) = builder(NodeType.FILE, name,
 fun NodeBuilder.PACKAGE(name: String, f: NodeBuilder.() -> Unit) = node(NodeType.PACKAGE, name, f)
 fun NodeBuilder.FILE(name: String, f: NodeBuilder.() -> Unit) = node(NodeType.FILE, name, f)
 fun NodeBuilder.CLASS(name: String, f: NodeBuilder.() -> Unit) = node(NodeType.CLASS, name, f)
-fun NodeBuilder.LAMBDA(name: String, f: NodeBuilder.() -> Unit) = node(NodeType.LAMBDA, name, f)
+fun NodeBuilder.LAMBDA(f: NodeBuilder.() -> Unit) = node(NodeType.LAMBDA, null, f)
 fun NodeBuilder.FUNCTION(name: String, f: NodeBuilder.() -> Unit) = node(NodeType.FUNCTION, name, f)
 
 fun builder(type: NodeType, name: String?, f: NodeBuilder.() -> Unit): NodeBuilder {
